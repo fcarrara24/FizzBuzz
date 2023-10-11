6d62d3b70fd4d8d521c7674643ac3141cbbcd87b
fcarrara24 <f.carrara24@studenti.unibg.it>
2023-10-11 16:35:03 +0200

    added a on-off swich to hide the table inside of the container

--- js/script.js
+++ js/script.js
@@ -5,41 +5,48 @@ start.addEventListener('click', function () {
     let print;
     let div;
     let color;
-    for (let i = 1; i <= 100; i++) {
-        //resetting addString & printstring
-        div = '';
-        print = '';
-        color = '';
-        if (i % 3 === 0) {
-            color = 'green';
-            print = 'Fizz';
-        }
-
-        if (i % 5 === 0) {
-            //if it was divided also by 3 it concat the string
-            if (color === 'green') {
-                color = 'red'
-            } else {
-                color = 'yellow';
+    console.log(container.innerHTML);
+    if (container.innerHTML === "") {
+        for (let i = 1; i <= 100; i++) {
+            //resetting addString & printstring
+            div = '';
+            print = '';
+            color = '';
+            if (i % 3 === 0) {
+                color = 'green';
+                print = 'Fizz';
             }
-            print = print + 'Buzz';
-        } else if (i % 3 !== 0) {
-            color = 'blue';
-            print = String(i);
+
+            if (i % 5 === 0) {
+                //if it was divided also by 3 it concat the string
+                if (color === 'green') {
+                    color = 'red'
+                } else {
+                    color = 'yellow';
+                }
+                print = print + 'Buzz';
+            } else if (i % 3 !== 0) {
+                color = 'blue';
+                print = String(i);
+            }
+
+            console.log(print);
+            //dinamically creating a div with standard class inside
+            div = document.createElement("div");
+
+            //attaching another class name
+            div.classList.add('myCard')
+            div.classList.add(color);
+
+            //inserting the right container content
+            div.textContent = print;
+
+            container.append(div)
         }
-
-        console.log(print);
-        //dinamically creating a div with standard class inside
-        div = document.createElement("div");
-
-        //attaching another class name
-        div.classList.add('myCard')
-        div.classList.add(color);
-
-        //inserting the right container content
-        div.textContent = print;
-
-        container.append(div)
+    } else {
+        //cleaning all inside content
+        container.innerHTML = "";
     }
+
 
 });
